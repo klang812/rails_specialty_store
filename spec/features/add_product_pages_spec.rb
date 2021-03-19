c69298f5332554ea43dd4f4c1a0d8e57c1f8e35c
Kerry Lang <klang812@gmail.com>
2021-03-19 15:13:44 -0700

    add integration test for add product test to pass

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "the add product process" do
+  it "adds a new product" do
+    review = Review.new({:author => 'Kerry', :content_body => '123456789101112131415161718192021222324252627282930', :rating => 5})
+    review.save
+    visit products_path
+    click_link 'Create new product'
+    fill_in 'Name', :with => 'Spaghetti'
+    fill_in 'Cost', :with => '6'
+    fill_in 'Country of origin', :with => 'USA'
+    click_on 'Create Product'
+    expect(page).to have_content 'Product successfully added!'
+    expect(page).to have_content 'Spaghetti'
+  end
+end
