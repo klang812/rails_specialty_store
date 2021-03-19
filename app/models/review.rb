ccb5c9e507c650b8e0b143b59d3352e5301e2fc2
Kerry Lang <klang812@gmail.com>
2021-03-19 15:02:16 -0700

    add titleize call back test to pass

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,7 +6,15 @@ class Review < ApplicationRecord
   validates_length_of :content_body, maximum: 250
   validates :rating, presence: true
   validates :rating, :inclusion => 1..5
-  
-  
+
+  # before_save(:titleize_review, presence: author)
+
+  # private
+  #   def titleize_review
+  #     self.author = self.author.titleize
+  #   end
   end
+  
+  
+  
 
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 describe Product do
   it{ should have_many(:reviews) }
+
+  it("titleizes the name of a product") do
+    product = Product.create({name: "cookies", cost: 5, country_of_origin: "USA"})
+    expect(product.name()).to(eq("Cookies"))
+  end
 end
