0b8fbaefe3a479dfce5e02e51d4a125e6c50bf99
Kerry Lang <klang812@gmail.com>
2021-03-21 13:30:53 -0700

    add edit review integrations test to pass

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -10,7 +10,7 @@ describe 'the add review process' do
     click_link 'Spaghetti'
     click_link 'Add a review'
     fill_in 'Author', :with => 'Kerry'
-    fill_in 'Review', :with => 'This spaghetti cooks perfectly al dente and with a bit of salt are delicious!'
+    fill_in 'Review', :with => 'This spaghetti cooks perfectly al dente and with a bit of salt is delicious!'
     fill_in 'Rating', :with => '5'
     click_on 'Create Review'
     expect(page).to have_content 'Review successfully added!'
