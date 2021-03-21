a6ea5fe24c944da1df12701d887fb51a21c93073
Kerry Lang <klang812@gmail.com>
2021-03-21 09:54:35 -0700

    add ratings range validation test to pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 describe Product do
-  it{ should have_many(:reviews) }
-  it{ should validate_presence_of(:name) }
-  it{ should validate_presence_of(:cost) }
-  it{ should validate_presence_of(:country_of_origin) }
 
-  it("titleizes the name of a product and country of origin") do
-    product = Product.create({name: "cookies", cost: 5, country_of_origin: "united states"})
-    expect([product.name, product.country_of_origin]).to(eq(["Cookies", "United States"]))
+  describe 'associations' do
+    it{ should have_many(:reviews) }
+  end
+
+  describe 'validations' do
+    it{ should validate_presence_of(:name) }
+    it{ should validate_presence_of(:cost) }
+    it{ should validate_presence_of(:country_of_origin) }
+  end
+
+  describe 'Product#titleize' do
+    it("titleizes the name of a product and country of origin") do
+      product = Product.create({name: "cookies", cost: 5, country_of_origin: "united states"})
+      expect([product.name, product.country_of_origin]).to(eq(["Cookies", "United States"]))
+    end
   end
 end
