bbb26cb5a5dbda63c50b6d1d6d07d7f8511d678f
Kerry Lang <klang812@gmail.com>
2021-03-21 09:31:32 -0700

    add cost validation test to pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Product do
   it{ should have_many(:reviews) }
   it{ should validate_presence_of(:name) }
+  it{ should validate_presence_of(:cost) }
 
   it("titleizes the name of a product and country of origin") do
     product = Product.create({name: "cookies", cost: 5, country_of_origin: "united states"})
